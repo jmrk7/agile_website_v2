2efd8e7bea790adf418d278c16f5c0123edc499f
dev0715 <toptal126@outlook.com>
2022-06-09 08:03:07 -0700

    fix import signup form error

--- pages/bumblebee/register.jsx
+++ pages/bumblebee/register.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import FormContainer from "/components/form-container"
 import SignUpHero from "/components/Bumblebee/SignUpHero"
-import SignUpForm from "/components/Bumblebee/SignupForm"
+import SignUpForm from "/components/Bumblebee/SignUpForm"
 
 import classnames from "classnames/bind"
 import * as styles from "./register.module.scss"
--- pages/dragonfly/register.jsx
+++ pages/dragonfly/register.jsx
@@ -2,14 +2,14 @@ import React from "react"
 
 import FormContainer from "/components/form-container"
 import SignUpHero from "/components/Dragonfly/SignUpHero"
-import SignUpForm from "/components/Dragonfly/SignupForm"
+import SignUpForm from "/components/Dragonfly/SignUpForm"
 
 import classnames from "classnames/bind"
 import * as styles from "./register.module.scss"
 
 const cx = classnames.bind(styles);
 
-const Register = () => {
+const Register = () => { 
   return (
     <div style={{backgroundColor: "#0d0f19"}}>      
       <div className={cx('signUpWrapper')}>
