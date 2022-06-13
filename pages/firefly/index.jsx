b0963fb7c66a6202184e6fc7be921807f6a39c24
dev0715 <toptal126@outlook.com>
2022-06-13 13:06:10 -0700

    add modal for firefly

--- pages/firefly/index.jsx
+++ pages/firefly/index.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import Header from "/components/Firefly/Header"
 import Content from "/components/Firefly/Content"
+import Modal from "/components/Firefly/Modal"
 
 const Firefly = () => {
   return (
     <div className="agiletestware_body firefly">
       <Header />
-      <Content />
+      <Content />      
+      <Modal/>
     </div>
   )
 }
