c2dfce4f6fb7030c616920374da1c213be932c71
dev0715 <toptal126@outlook.com>
2022-11-28 08:08:29 -0500

    update env

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
     return config;
   },
   env: {
-    RECAPTCHA_SITE_KEY: "6LeP_VUUAAAAAMMRcWjs5zlkCVvebQiOOMWdERZs",
-    DISABLE_RECAPTCHA: false,
-    SLACK_API_BASE_URL: "https://hooks.slack.com/services/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",
-    FRESHSALES_API_KEY: "YnCKDaGhjVKMeZ_5V0XH2w",
-    FRESHSALES_API_URL: "http://agiletestware.freshsales.io/api/leads/",
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
